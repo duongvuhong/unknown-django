74f2632d8782505f985a891c5e5e5a96aeaa43e6
Vu Hong Duong <duong.vuhong.uet@gmail.com>
2020-09-26 17:44:33 +0900

    Add sample unit test

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from .models import Topic
+
 # Create your tests here.
+
+class TopicModelTests(TestCase):
+    def test_text_field_with_valid_length(self):
+        topic = Topic(text='This is valid topic')
+
+        self.assertEqual(topic.text, 'This is valid topic')
